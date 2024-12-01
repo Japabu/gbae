98b39c1167cc39a9d24c4d3c7d66df95a0d989b8
japabu <japa.busse@gmail.com>
2024-12-01 12:26:59 +0100

    refactor

--- src/cpu/instructions/mod.rs
+++ src/cpu/instructions/mod.rs
@@ -2,10 +2,9 @@ use crate::bitutil::get_bit;
 
 use super::CPU;
 
-pub mod lut;
-mod dp_lut;
 mod dp;
 mod branch;
+pub mod lut;
 
 fn set_nz_flags(cpu: &mut CPU, value: u32) {
     cpu.r.set_negative_flag(get_bit(value, 31));
